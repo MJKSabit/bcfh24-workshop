3b283a10e84e522e96a8ac52d642d92890c36eba
Tamim Ehsan <tamimehsan99@gmail.com>
2024-10-11 01:34:11 +0600

    initialized spring boot product-service

---
+++ services/product-service/src/test/java/com/buetcsefest2024/workshop/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buetcsefest2024.workshop.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
